01def32f0e7b34b4461e78e9d72d9bb204382f64
Bhavyaa Arora <44175278+Bhavyaa08@users.noreply.github.com>
2020-07-06 10:09:45 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Api is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server is up and running on", PORT);
+});
